006040047222384ba27fd35adca2db0c4af58baf
FolusoFayomi <foluso.fayomi@tri-c.edu>
2023-02-23 22:02:10 -0500

    Files with changes and comments

---
+++ Week 5 Lab/Corrections Lab5-Arthur/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArthurMusic_Lounge.Models
+{
+    public class ContactModel
+    {
+        //add ? to each data type to make them nullable for data validation to work properly
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Address { get; set; }
+
+        [Required]
+        public string? Phone { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Message { get; set; }
+    }
+}
---
+++ Week 5 Lab/Corrections Lab5-Arthur/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ArthurMusic_Lounge.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ArthurMusic_Lounge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        //We don't want it here
+        //public IActionResult Contact()
+        //{
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
